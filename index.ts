ded9ca9f5548c47523c73d73af94c985377205db
kunal00000 <kunalverma2468@gmail.com>
2025-02-22 16:00:48 +0530

    faet: init websockets

--- index.ts
+++ index.ts
@@ -1,17 +1,48 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import { createPage, spawnBrowser } from "./src/browser";
-import { agent } from "./src/agent";
+import { Hono } from "hono";
+import { createBunWebSocket } from "hono/bun";
+import type { ServerWebSocket } from "bun";
 
-const INIT_URL = "https://www.google.com";
+const app = new Hono();
+const { upgradeWebSocket, websocket } = createBunWebSocket<ServerWebSocket>();
 
-async function main() {
-  const browser = await spawnBrowser();
-  const page = await createPage(INIT_URL, browser);
+app.get(
+  "/ws",
+  upgradeWebSocket((c) => {
+    return {
+      onOpen: (_event, ws) => {
+        ws.send("Connection established");
+      },
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`);
+        ws.send("Hello from server!");
+      },
+      onClose: () => {
+        console.log("Connection closed");
+      },
+    };
+  })
+);
 
-  await agent(page);
+export default {
+  fetch: app.fetch,
+  websocket,
+};
 
-  await browser.close();
-}
+// ----------------- 1. Basic Puppeteer -----------------
+// import { createPage, spawnBrowser } from "./src/browser";
+// import { agent } from "./src/agent";
 
-main();
+// const INIT_URL = "https://www.google.com";
+
+// async function main() {
+//   const browser = await spawnBrowser();
+//   const page = await createPage(INIT_URL, browser);
+
+//   await agent(page);
+
+//   await browser.close();
+// }
+
+// main();
